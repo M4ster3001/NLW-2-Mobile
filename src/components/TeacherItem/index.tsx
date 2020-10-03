258a7fc622c5d8cf9672d86e0a0b2f0bb134d5b4
Aldo Junior <aldocfjunior@gmail.com>
2020-10-03 10:21:44 -0300

    Att expo

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,11 @@ import { Image, Text, View } from 'react-native'
 
 import styles from './styles'
 
+import heartOutLine from '../../assets/images/icons/heart-outline.png'
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+import { RectButton } from 'react-native-gesture-handler'
+
 function TeacherItem() {
 
     return (
@@ -14,8 +19,33 @@ function TeacherItem() {
                 />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.nome}>Aldo</Text>
+                    <Text style={styles.name}>Aldo</Text>
                     <Text style={styles.subject}>Química</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Testando a parte de componente TeacherItem bio
+                {'\n'}
+                Teste som testando 1 2 3
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/ hora {'   '}
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        <Image source={heartOutLine} />
+                        <Image source={unFavoriteIcon} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
                 </View>
             </View>
         </View>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TextInput } from 'react-native'
+import { BorderlessButton, ScrollView } from 'react-native-gesture-handler'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
@@ -7,11 +8,60 @@ import TeacherItem from '../../components/TeacherItem'
 import styles from './styles'
 
 function TeacherList() {
-    return <View style={styles.container} >
-        <PageHeader title="Proffys disponíveis" />
+    const [ isFiltersVisible, setIsFiltersVisible ] = useState(true)
 
-        <TeacherItem />
+    return (
+    <View style={styles.container} >
+        <PageHeader 
+            title="Proffys disponíveis"
+        >
+        { isFiltersVisible && ( 
+            <View style={styles.searchForm} >
+                <Text style={styles.label}>Matéria</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Qual a matéria?"
+                    placeholderTextColor="#c1bccc"
+                />
+
+                <View style={styles.inputGroup}>
+                    <View style={styles.inputBlock}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Qual o dia?"
+                            placeholderTextColor="#c1bccc"
+                        />
+                    </View>
+
+                    <View style={styles.inputBlock}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput 
+                            textContentType="password"
+                            style={styles.input}
+                            placeholder="Qual o horário?"
+                            placeholderTextColor="#c1bccc"
+                        />
+                    </View>
+                </View>
+            </View>
+        )}
+        </PageHeader>
+
+        <ScrollView
+            style={styles.teacherList}
+            contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16
+            }}
+        >
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+        </ScrollView>
     </View>
+    )
 }
 
 export default TeacherList
